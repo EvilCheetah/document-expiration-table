bb3deb275eb51e55a99ca4fd4b05f6301c33749a
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-06-24 14:54:05 -0700

    add: expiration table component

--- src/components/ExpirationTable/ExpirationTable.tsx
+++ src/components/ExpirationTable/ExpirationTable.tsx
@@ -1,9 +1,28 @@
-import { VehicleProps } from "./Vehicle.interface";
+import './ExpirationTable.css';
+import { TableHeader } from "./TableHeader";
+import { TableBody } from './TableBody';
 
 
-export function ExpirationTable({unit_number}: VehicleProps)
+export function ExpirationTable({ vehicles })
 {
+    const table_header = [
+        { label: 'Unit',                      accessor: 'unit',                 },
+        { label: 'Driver\'s Name',            accessor: 'driver_name',          },
+        { label: 'Driver\'s License',         accessor: 'unit',                 },
+        { label: 'Authorization Documents',   accessor: 'authorization',        },
+        { label: 'Registration',              accessor: 'registration',         },
+        { label: 'Annual Inspection',         accessor: 'annual_inspection',    },
+        { label: 'Insurance: General',        accessor: 'insurance_general',    },
+        { label: 'Insurance: Cargo',          accessor: 'insurance_cargo',      },
+        { label: 'Insurance: Liability',      accessor: 'insurance_liability',  },
+        { label: 'Insurance: Auto',           accessor: 'insurance_auto'        }
+    ];
+
     return (
-        <div>Hello World</div>
+        <table>
+            <caption>Document Expiration Table</caption>
+            <TableHeader columns={table_header} />
+            <TableBody   vehicles={vehicles} />
+        </table>
     );
 }
