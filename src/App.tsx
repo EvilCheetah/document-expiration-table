1d71d882092c4d6794b0ac82d355851e024ba3b2
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-06-24 14:50:43 -0700

    add: import data and passed it as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { ExpirationTable } from './components/ExpirationTable/ExpirationTable';
+import vehicle_information from './data.json'
+
 
 function App()
 {
     return (
         <div className="App">
-            <ExpirationTable unit_number={''} />
+            <ExpirationTable vehicles={vehicle_information}/>
         </div>
     );
 }
