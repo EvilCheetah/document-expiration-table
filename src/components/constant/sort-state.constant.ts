f04f50434ff301492d0ae55db5d316a56201b146
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-06-28 15:05:20 -0700

    add: sort state constant

---
+++ src/components/constant/sort-state.constant.ts
@@ -0,0 +1,12 @@
+export const INIT_SORT_STATE = {
+    unit:                    null,
+    driver_name:             null,
+    driver_license:          null,
+    authorization:           null,
+    registration:            null,
+    annual_inspection:       null,
+    insurance_general:       null,
+    insurance_cargo:         null,
+    insurance_liability:     null,
+    insurance_auto:          null
+}
