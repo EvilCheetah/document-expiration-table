77d5311caa110594d06bb56023f4d0f3d9df1e3e
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-06-28 15:06:47 -0700

    update: headers

--- src/components/ExpirationTable/ExpirationTable.tsx
+++ src/components/ExpirationTable/ExpirationTable.tsx
@@ -1,27 +1,33 @@
 import './ExpirationTable.css';
 import { TableHeader } from "./TableHeader";
 import { TableBody } from './TableBody';
+import { useState } from 'react';
+import { INIT_SORT_STATE } from '../constant/sort-state.constant';
 
 
 export function ExpirationTable({ vehicles })
 {
+    const sorted_vehicles = {...vehicles}
+
+    const [sorting_values, setSortingValues] = useState(INIT_SORT_STATE)
+
     const table_header = [
-        { label: 'Unit',                      accessor: 'unit',                 },
-        { label: 'Driver\'s Name',            accessor: 'driver_name',          },
-        { label: 'Driver\'s License',         accessor: 'unit',                 },
-        { label: 'Authorization Documents',   accessor: 'authorization',        },
-        { label: 'Registration',              accessor: 'registration',         },
-        { label: 'Annual Inspection',         accessor: 'annual_inspection',    },
-        { label: 'Insurance: General',        accessor: 'insurance_general',    },
-        { label: 'Insurance: Cargo',          accessor: 'insurance_cargo',      },
-        { label: 'Insurance: Liability',      accessor: 'insurance_liability',  },
-        { label: 'Insurance: Auto',           accessor: 'insurance_auto'        }
+        { label: 'Unit',                    accessor: 'unit',                sorting_state: sorting_values.unit },
+        { label: 'Driver\'s Name',          accessor: 'driver_name',         sorting_state: sorting_values.driver_name },
+        { label: 'Driver\'s License',       accessor: 'driver_license',      sorting_state: sorting_values.driver_license },
+        { label: 'Authorization Documents', accessor: 'authorization',       sorting_state: sorting_values.authorization },
+        { label: 'Registration',            accessor: 'registration',        sorting_state: sorting_values.registration },
+        { label: 'Annual Inspection',       accessor: 'annual_inspection',   sorting_state: sorting_values.annual_inspection },
+        { label: 'Insurance: General',      accessor: 'insurance_general',   sorting_state: sorting_values.insurance_general },
+        { label: 'Insurance: Cargo',        accessor: 'insurance_cargo',     sorting_state: sorting_values.insurance_cargo },
+        { label: 'Insurance: Liability',    accessor: 'insurance_liability', sorting_state: sorting_values.insurance_liability },
+        { label: 'Insurance: Auto',         accessor: 'insurance_auto',      sorting_state: sorting_values.insurance_auto }
     ];
 
     return (
-        <table>
-            <caption>Document Expiration Table</caption>
-            <TableHeader columns={table_header} />
+        <table className='table-fill'>
+            <caption className='table-title'>Document Expiration Table</caption>
+            <TableHeader columns={table_header} setSortingValues={setSortingValues}/>
             <TableBody   vehicles={vehicles} />
         </table>
     );
