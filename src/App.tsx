f765cd77f404d9f5f44813c6a44b85fb7e3ea806
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-07-06 15:09:55 -0700

    add: state for vehicles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import { ExpirationTable } from './components/ExpirationTable/ExpirationTable';
 import vehicle_information from './data.json'
@@ -5,9 +6,11 @@ import vehicle_information from './data.json'
 
 function App()
 {
+    const [ vehicles, setVehicles ] = useState(vehicle_information);
+
     return (
         <div className="App">
-            <ExpirationTable vehicles={vehicle_information}/>
+            <ExpirationTable vehicles={vehicles} setVehicles={setVehicles}/>
         </div>
     );
 }
